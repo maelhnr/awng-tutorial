3b5f0f40d25d0eb576fc315335089e2ec827a52a
Maël Honoré <mael.honore@student.isae-supaero.fr>
2025-03-28 17:23:37 +0100

    Fin partie 6 templates et admin

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -2,6 +2,7 @@
 from django.db import models
 import datetime
 from django.utils import timezone
+from django.contrib import admin
 
 "Permet d'intéragir avec la base de données"
 "!! quand on modifie le models.py, il faut migrate pour l'appliquer à la base de données pour la rendre compatible"
@@ -12,6 +13,11 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
     
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
